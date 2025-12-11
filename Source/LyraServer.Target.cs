1a811da3fcab1a4eb89d0a1f76f1ba9bcd6292ed
aigege <2221068116@qq.com>
2025-12-11 21:59:26 +0800

    编译模块的功能性添加

--- Source/LyraServer.Target.cs
+++ Source/LyraServer.Target.cs
@@ -8,8 +8,9 @@ public class LyraServerTarget : TargetRules
 	public LyraServerTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+	
 		ExtraModuleNames.Add("LyraGame");
+		
+		LyraGameTarget.ApplySharedLyraTargetSettings(this);
 	}
 }
